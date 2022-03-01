5e5313585c0dee2d013824cd772e88fd8cd9e511
edubs <11930020+edubs@users.noreply.github.com>
2022-03-01 15:31:56 -0500

    codewwars gap in primes

--- 005-prime-gap/prime-gap.go
+++ 005-prime-gap/prime-gap.go
@@ -1,13 +1,72 @@
+// Gap in Primes https://www.codewars.com/kata/561e9c843a2ef5a40c0000a4/go
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func Gap(g, m, n int) []int {
+	var pir []int
+	for _, prime := range SieveOfEratosthenes(n + 1) {
+		if prime >= m {
+			pir = append(pir, prime)
+		}
+	}
 
-	return []int{3, 5}
+	if len(pir) < 2 {
+		return nil // list of valid primes is less than two therefore no gap can be calculated, return nil
+	} else if testPair(pir[0], pir[1], g) {
+		return pir[0:2] // if the length is at least two, and the first two produce a match, no need to go further
+	} else {
+		low := pir[0]
+		high := pir[1]
+		for index, value := range pir {
+			if index+1 > len(pir) {
+				fmt.Println("reached end")
+			}
+			if testPair(low, high, g) {
+				pir = []int{low, high}
+				return pir
+			} else {
+				low = high
+				high = value
+			}
+		}
+	}
+	return nil
+}
 
+func testPair(a int, b int, c int) bool {
+	if b-a == c {
+		return true
+	}
+
+	return false
+}
+
+// https://www.thepolyglotdeveloper.com/2016/12/determine-number-prime-using-golang/
+func SieveOfEratosthenes(value int) []int {
+	f := make([]bool, value)
+	var listOfPrimes = []int{}
+	for i := 2; i <= int(math.Sqrt(float64(value))); i++ {
+		if f[i] == false {
+			for j := i * i; j < value; j += i {
+				f[j] = true
+			}
+		}
+	}
+	for i := 2; i < value; i++ {
+		if f[i] == false {
+			listOfPrimes = append(listOfPrimes, i)
+		}
+	}
+	return listOfPrimes
 }
 
 func main() {
-	fmt.Print(Gap(2, 3, 50))
+	fmt.Println(Gap(2, 5, 7))
+	fmt.Println(Gap(2, 100, 110))
+	fmt.Println(Gap(4, 100, 110))
+	fmt.Println(Gap(10, 300, 400))
 }
--- 005-prime-gap/prime-gap_test.go
+++ 005-prime-gap/prime-gap_test.go
@@ -1,20 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"strings"
+	"reflect"
 	"testing"
 )
 
 func TestPrimeGap(t *testing.T) {
-	got := Gap(2, 3, 50)
-	want := []int{3, 5}
-
-	if Converter(got) != Converter(want) {
-		t.Errorf("got %q want %q", got, want)
+	tables := []struct {
+		g    int
+		m    int
+		n    int
+		want []int
+	}{
+		{2, 5, 7, []int{5, 7}},
+		{2, 100, 110, []int{101, 103}},
+		{4, 100, 110, []int{103, 107}},
+		{6, 100, 110, nil},
+		{8, 300, 400, []int{359, 367}},
+		{10, 300, 400, []int{337, 347}},
 	}
-}
 
-func Converter(i []int) string {
-	return strings.Trim(strings.Join(strings.Fields(fmt.Sprint(i)), ", "), "[]")
+	for _, table := range tables {
+		got := Gap(table.g, table.m, table.n)
+		if !reflect.DeepEqual(got, table.want) {
+			t.Errorf("Wanted: %v Got: %v", table.want, got)
+		}
+	}
 }
